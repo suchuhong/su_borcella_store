17044dd6b6da75fa3c8182943e1ddd2e275127bd
suchuhong <1442615457@qq.com>
2024-10-03 14:58:59 +0800

    配置clerk管理登录注册

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import '../globals.css'
+
+import { ClerkProvider } from '@clerk/nextjs'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'su Borcelle Store',
+  description: 'su Borcelle Ecommerce store',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ClerkProvider>{children}</ClerkProvider>
+      </body>
+    </html>
+  )
+}
---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from '@clerk/nextjs'
+
+export default function Home() {
+  return (
+    <div>
+      Home Page
+      <UserButton />
+    </div>
+  )
+}
