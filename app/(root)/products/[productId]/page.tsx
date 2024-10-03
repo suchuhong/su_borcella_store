34a91c500894091199e2b848b1225545df86ba97
suchuhong <1442615457@qq.com>
2024-10-03 21:12:39 +0800

    商品详情设计

---
+++ app/(root)/products/[productId]/page.tsx
@@ -0,0 +1,26 @@
+import Gallery from '@/components/Gallery'
+import ProductCard from '@/components/ProductCard'
+import ProductInfo from '@/components/ProductInfo'
+import { getProductDetails } from '@/lib/actions/actions'
+
+const ProductDetails = async ({
+  params,
+}: {
+  params: { productId: string }
+}) => {
+  const productDetails = await getProductDetails(params.productId)
+
+  return (
+    <>
+      <div className="flex justify-center items-start gap-16 py-10 px-5 max-md:flex-col max-md:items-center">
+        {/* 展示图片 */}
+        <Gallery productMedia={productDetails.media} />
+        <ProductInfo productInfo={productDetails} />
+      </div>
+    </>
+  )
+}
+
+export const dynamic = 'force-dynamic'
+
+export default ProductDetails
